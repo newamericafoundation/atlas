8187003bfffd307f6800eeff2bb9d5682f01ddcb
Peter Szerzo <szerzo.peter@gmail.com>
2015-05-25 12:44:47 -0400

    changed port

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ var express = require('express'),
 	router = require('./routes/index'),
 	basicAuth = require('basic-auth-connect');
 
-//mongoose.connect('mongodb://ec2-52-25-41-189.us-west-2.compute.amazonaws.com:27017/mongoid', function(err) {
-//	if (err) { return console.log(err); }
-//});
+mongoose.connect('mongodb://ec2-52-25-41-189.us-west-2.compute.amazonaws.com:27017/mongoid', function(err) {
+	if (err) { return console.log(err); }
+});
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
@@ -18,4 +18,4 @@ app.use(basicAuth('nafed', 'nafed148'));
 
 app.use(router);
 
-app.listen(3001);
+app.listen(8081);
