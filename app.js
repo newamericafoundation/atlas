841f02a8cc902f223b1d4372e3cc01ffdf67b933
Peter Szerzo <szerzo.peter@gmail.com>
2015-05-25 10:49:48 -0400

    handled mongoose connectione rror

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var express = require('express'),
 	router = require('./routes/index'),
 	basicAuth = require('basic-auth-connect');
 
-mongoose.connect('mongodb://localhost:27017/mongoid');
+mongoose.connect('mongodb://ec2-52-25-41-189.us-west-2.compute.amazonaws.com:27017/mongoid', function(err) {
+	if (err) { return console.log(err); }
+});
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
