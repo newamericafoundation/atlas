6dbf60660abaa6ce23f9afc39a83f08812a428ec
Peter Szerzo <szerzo.peter@gmail.com>
2015-05-26 12:17:56 -0400

    cleaned up console messages

--- app.js
+++ app.js
@@ -11,21 +11,21 @@ var db_url = {
 	'prod': 'ec2-52-25-41-189.us-west-2.compute.amazonaws.com'
 };
 
-mongoose.connect('mongodb://' + db_url.dev + ':27017/mongoid', function(err) {
-	if (err) { return console.log(err); }
+mongoose.connect('mongodb://' + db_url.prod + ':27017/mongoid', function(err) {
+	if (err) { return console.dir(err); }
 });
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
-// serve gzip if it is available
+// Serve gzip if it is available
 app.get([ '*.js' ], function(req, res, next) {
 	var gzipUrl = 'public' + req.url + '.gz';
 	fs.readFile(gzipUrl, function(err) { 
 		if (err) { return next(); }
 		req.url = req.url + '.gz';
 		res.set('Content-Encoding', 'gzip');
-		console.log('gzip found and served for: ' + req.url)
+		console.log('Found and served gzip version for: ' + req.url)
 		next();
 	});
 });
@@ -36,4 +36,7 @@ app.use(basicAuth('nafed', 'nafed148'));
 
 app.use(router);
 
-app.listen(8081);
+app.listen(8081, function(err) { 
+	if(err) { return console.log(err); }
+	console.log('Listening on port 8081.')
+});
