2b17294feb58240da1bb7bd53cf9e6a755f9ba8d
Peter Szerzo <szerzo.peter@gmail.com>
2016-07-30 10:25:50 +0200

    Updated .env, provided .env.example for version control

--- db/connector.js
+++ db/connector.js
@@ -2,12 +2,9 @@ import { MongoClient } from 'mongodb'
 
 export default new Promise((resolve, reject) => {
 
-	var { NODE_ENV, PRODUCTION_DB_URL } = process.env
+	var { NODE_ENV, MONGODB_URI } = process.env
 
-	var dbUrlBase = (NODE_ENV === 'development') ? 'localhost' : PRODUCTION_DB_URL
-	var dbUrl = `mongodb://${dbUrlBase}:27017/mongoid`
-
-	MongoClient.connect(dbUrl, (err, db) => {
+	MongoClient.connect(MONGODB_URI, (err, db) => {
 		if (err) {
 			console.log('Unable to connect to the database.')
 			console.log(err)
